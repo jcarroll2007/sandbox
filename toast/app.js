cab775e9ee64a889998cb9baf0635a9d0e8d203a
Jordan Carroll <jcarroll42@coop-490-z420.elsys.private>
2015-03-13 08:22:44 -0400

    Toast: can't remember what this commit adds - see diff.

--- toast/app.js
+++ toast/app.js
@@ -5,3 +5,13 @@ angular.module('myApp', [])
 			template: '<p class="hello-world">Hello World!</p>'
 		};
 	});
+
+
+// sudo code
+
+// parent is the object that the target is being deleted from,
+// target is the object that is being deleted
+// id
+function confirmDelete(parent, target, id) {
+
+}
